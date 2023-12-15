64f9cb666df314ff88b3775b60fd934ebd52c818
TwistingGround <karla.duplic2@gmail.com>
2023-12-15 23:44:13 +0100

    first commit

---
+++ imagedetection.py
@@ -0,0 +1,37 @@
+import sys
+import os
+venv_path = 'venv\Lib\site-packages'
+sys.path.insert(0, venv_path)
+
+import cv2
+from deepface import DeepFace
+from imspecifications import specifications
+
+img_path = 'images\young_girl.jpg'
+
+detected_faces = DeepFace.extract_faces(img_path)  #detects face in image
+attributes = DeepFace.analyze(img_path, actions = ['gender', 'age', 'race', 'emotion'])  #analyzes specified attributes
+
+#print(attributes)
+
+if detected_faces:    #checks if any faces were detected
+
+    img = cv2.imread(img_path)
+
+    for face in detected_faces:   #put a bounding box around each detected face
+
+        face1 = face.get('facial_area')    
+
+        if face1:
+            x, y, w, h = face1['x'], face1['y'], face1['w'], face1['h']
+
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            specifications(img_path, img, x, y, h)
+
+    cv2.imshow('Detected Faces', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+else:
+    print('No faces were detected in the provided image.')
---
+++ imspecifications.py
@@ -0,0 +1,35 @@
+import sys
+import os
+venv_path = 'venv\Lib\site-packages'
+sys.path.insert(0, venv_path)
+
+import cv2
+from deepface import DeepFace
+
+def specifications(img_path, img, x, y, h):   #puts text on image
+    attributes = DeepFace.analyze(img_path, actions = ['gender', 'age', 'race', 'emotion'])
+
+    if attributes:
+                for specs in attributes:
+
+                    #extract info from dictionary inside a list
+                    gender = specs['dominant_gender']       
+                    age = specs['age']
+                    race = specs['dominant_race']
+                    emotion = specs['dominant_emotion']
+
+                    text1 = f'Gender: {gender}'
+                    text2 = f'Age: {age}'
+                    text3 = f'Race: {race}'
+                    text4 = f'Emotion: {emotion}'
+
+                    text = [text1, text2, text3, text4]
+                    
+                    i = 0
+                    c = 60
+                    while i<len(text):
+                            cv2.putText(img, text[i], (x,y+h+c), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1) #puts each attribute in its own line
+                            i+=1
+                            c+=30
+    else:
+         print("Failed to analyze face.")
